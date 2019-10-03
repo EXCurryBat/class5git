47e6ee2edca0a37b990f4bdad4ec85442c34a09a
EXcurrybat <dxiao5@my.bcit.ca>
2019-10-03 09:24:51 -0700

    Add project files.

---
+++ class5git/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace class5git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
